a510c4667dcc2e6da1dfc9321581d9c9a4baef11
Robby Llena <robby.llena@gmail.com>
2018-11-20 01:53:08 +0800

    Small Changes

--- src/main/java/com/llena/weather/api/WeatherController.java
+++ src/main/java/com/llena/weather/api/WeatherController.java
@@ -1,6 +1,6 @@
 package com.llena.weather.api;
 
-import org.springframework.http.ResponseEntity;
+import com.llena.weather.object.Weather;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +13,17 @@ public class WeatherController {
     private final String CURRENT_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q={city}&appid={appid}";
 
     @RequestMapping("/weather")
-    public ResponseEntity<String> getWeather(@RequestParam("city") String city) {
+    public String getWeather(@RequestParam("city") String city) {
         return getOpenWeatherResponse(city);
     }
 
-    public ResponseEntity<String> getOpenWeatherResponse(String city)
+    public String getOpenWeatherResponse(String city)
     {
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForEntity(CURRENT_WEATHER_URL, String.class, city, APPID);
+        Weather weather = restTemplate.getForObject(CURRENT_WEATHER_URL, Weather.class, city, APPID);
+
+
+
+        return weather.getWeather() + " " + weather.getDescription();
     }
 }
--- src/main/java/com/llena/weather/object/Weather.java
+++ src/main/java/com/llena/weather/object/Weather.java
@@ -1,4 +1,51 @@
 package com.llena.weather.object;
 
-public class Weather {
+import java.io.Serializable;
+
+public class Weather implements Serializable {
+    private long id;
+    private String location;
+    private String weather;
+    private String description;
+    private int temperature;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getWeather() {
+        return weather;
+    }
+
+    public void setWeather(String weather) {
+        this.weather = weather;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
 }
